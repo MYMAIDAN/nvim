f4b076c33f4da24fff19412a0eb1cc4b8b2d9ec0
Mykhailo Maidan <mykhailo.maidan@yalantis.net>
2023-06-25 13:02:15 -0400

    Add gitui instance creation each time exec

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -31,24 +31,34 @@ local docker_client = Terminal:new {
   },
 }
 
-
+function M.gitui_client_toggle()
 local gitui_client = Terminal:new {
-    cmd = "gitui",
-    dir = "git_dir",
-    hidden = true,
-    direction = "float",
-    float_opts = {
-        border = "double",
-    },
+  cmd = "gitui",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
 }
+gitui_client:toggle()
+end
 
 function M.docker_client_toggle()
   docker_client:toggle()
 end
 
-function M.gitui_client_toggle()
-    gitui_client:toggle()
-end
+--function M.gitui_client_toggle()
+--    gitui_client:toggle()
+--end
 
 
 return M
